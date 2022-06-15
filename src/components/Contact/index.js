c4f83b6709e3a860c7bf759e98aba93e863850fa
gpay <gpaytakov@gmail.com>
2022-06-15 13:07:16 -0500

    control state with a controlled component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,44 +1,58 @@
 import React, { useState } from "react";
 
 function ContactForm() {
-    const { name, email, message } = formState;
     const [formState, setFormState] = useState({
-      name: "",
-      email: "",
-      message: "",
+        name: "",
+        email: "",
+        message: "",
     });
+    const { name, email, message } = formState;
+
 
     function handleChange(e) {
-      setFormState({ ...formState, name: e.target.value });
+      setFormState({ ...formState, [e.target.name]: e.target.value });
     }
 
-    console.log(formState);
+    function handleSubmit(e) {
+      e.preventDefault();
+      console.log(formState);
+    }
     
     // JSX
     return (
-    <section>
+      <section>
         <h1>Contact me</h1>
-        <form id="contact-form">
-        <div>
+        <form id="contact-form" onSubmit={handleSubmit}>
+          <div>
             <label htmlFor="name">Name:</label>
             <input
-            type="text"
-            defaultValue={name}
-            onChange={handleChange}
-            name="name"
+              type="text"
+              defaultValue={name}
+              onChange={handleChange}
+              name="name"
             />
-        </div>
-        <div>
+          </div>
+          <div>
             <label htmlFor="email">Email address:</label>
-            <input type="email" defaultValue={email} name="email" />
-        </div>
-        <div>
+            <input
+              type="email"
+              defaultValue={email}
+              name="email"
+              onChange={handleChange}
+            />
+          </div>
+          <div>
             <label htmlFor="message">Message:</label>
-            <textarea name="message" defaultValue={message} rows="5" />
-        </div>
-        <button type="submit">Submit</button>
+            <textarea
+              name="message"
+              defaultValue={message}
+              onChange={handleChange}
+              rows="5"
+            />
+          </div>
+          <button type="submit">Submit</button>
         </form>
-    </section>
+      </section>
     );
 }
 
